edcb28a45e556e0e4be8f2887daf04692e129e66
fiatclone <165413501+FiatClone@users.noreply.github.com>
2025-07-31 19:58:54 +0700

    Update Layout.jsx

--- src/components/core/Layout.jsx
+++ src/components/core/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme } from '../contexts/ThemeContext';
+import { useTheme } from '../../contexts/ThemeContext';
 import Sidebar from './Sidebar';
 import MobileSidebar from './MobileSidebar';
 
