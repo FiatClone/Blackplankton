cf99fc43430c5368013159bcf8ec3faf9345856c
fiatclone <165413501+FiatClone@users.noreply.github.com>
2025-07-31 18:34:31 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import ComicPage from './pages/ComicPage';
 import StakePage from './pages/StakePage';
 import SwapPage from './pages/SwapPage';
 import LiquidityPage from './pages/LiquidityPage';
-import GovernancePage from './pages/GovernancePage';
+import GovernancePage from './pages/GavernancePage';
 import ErrorPage from './pages/ErrorPage';
 import NotificationContainer from './components/ui/Notification/NotificationContainer';
 
